d2b1872108105c7c83844d246e4f3d40ddd7864b
Emmanuel Raviart <emmanuel@raviart.com>
2015-05-28 09:46:30 +0200

    Initial commit

--- src/GitLegistique.jl
+++ src/GitLegistique.jl
@@ -1,5 +1,166 @@
+# GitLegistique.jl -- Convert Git diffs of law to french wordings
+# By: Emmanuel Raviart <emmanuel.raviart@data.gouv.fr>
+#
+# Copyright (C) 2015 Etalab
+# https://github.com/etalab/GitLegistique.jl
+#
+# The GitLegistique.jl package is licensed under the MIT "Expat" License.
+
+
 module GitLegistique
 
-# package code goes here
+
+using ArgParse
+using Compat
+using LibGit2
+import LibGit2: text
+using TextWrap
+
+
+include("articles.jl")
+
+
+function main()
+  args = parse_command_line()
+
+  repository = repo_discover(args["dir"])
+  old_commit = revparse(repository, args["old"])
+  new_commit = revparse(repository, args["new"])
+  diff = LibGit2.diff(repository, old_commit, new_commit)
+  articles = BuiltArticle[]
+  for patch in patches(diff)
+    delta = LibGit2.delta(patch)
+    if delta.old_file.path == delta.new_file.path
+      hunks = LibGit2.hunks(patch)
+      if length(hunks) == 1
+        # When there is only one hunk it may be the creation or deletion of an article.
+        hunk = hunks[1]
+        if hunk.old_start == 0 && hunk.old_lines == 0
+          # Create article.
+          println("Create article $(delta.new_file.path).")
+          new_blob = lookup(repository, delta.new_file.oid)
+          new_text = text(new_blob)
+          push!(articles, CreateArticles(parse_article(new_text)))
+          continue
+        end
+        if hunk.new_start == 0 && hunk.new_lines == 0
+          # Delete article.
+          println("Delete article $(delta.old_file.path).")
+          old_blob = lookup(repository, delta.old_file.oid)
+          old_text = text(old_blob)
+          push!(articles, DeleteArticles(parse_article(old_text)))
+          continue
+        end
+      end
+
+      # Modify article.
+      println("Modify article $(delta.new_file.path).")
+      old_blob = lookup(repository, delta.old_file.oid)
+      old_text = text(old_blob)
+      old_article = parse_article(old_text)
+      new_blob = lookup(repository, delta.new_file.oid)
+      new_text = text(new_blob)
+      new_article = parse_article(new_text)
+      for hunk in hunks
+        addition_encountered = false
+        new_first_paragraph_index = 0
+        new_last_paragraph_index = -1
+        old_first_paragraph_index = 0
+        old_last_paragraph_index = -1
+        for line in lines(hunk)
+          if line.line_origin == :addition
+            addition_encountered = true
+            new_last_paragraph_index = char_index_to_paragraph_index(new_article, line.content_offset + 1)
+            if new_first_paragraph_index == 0
+              new_first_paragraph_index = new_last_paragraph_index
+            end
+          elseif line.line_origin == :context
+            if addition_encountered
+              if old_first_paragraph_index == 0
+                # Hunk contains no deletion.
+                old_first_paragraph_index = line_index_to_paragraph_index(old_article, line.old_lineno - 1)
+                old_last_paragraph_index = old_first_paragraph_index - 1
+              end
+            else
+              # A first deletion may be on the next line.
+              old_first_paragraph_index = line_index_to_paragraph_index(old_article, line.old_lineno + 1)
+              old_last_paragraph_index = old_first_paragraph_index - 1
+            end
+          elseif line.line_origin == :deletion
+            old_last_paragraph_index = char_index_to_paragraph_index(old_article, line.content_offset + 1)
+            if old_first_paragraph_index == 0
+              old_first_paragraph_index = old_last_paragraph_index
+            end
+          end
+        end
+        push!(articles, ModifyArticle(old_article, old_first_paragraph_index:old_last_paragraph_index,
+          new_article, new_first_paragraph_index:new_last_paragraph_index))
+      end
+    else
+      # Move/rename article.
+      println("Rename article $(delta.old_file.path) to $(delta.new_file.path).")
+      println("TODO")
+      for hunk in hunks
+        for line in lines(hunk)
+          @show line.line_origin
+          @show line.old_lineno
+          @show line.new_lineno
+          @show line.content_offset
+          @show line.content
+        end
+      end
+      stat = LibGit2.stat(patch)
+      @show stat.adds
+      @show stat.dels
+    end
+  end
+
+  # Optimize articles.
+  optimized_articles = BuiltArticle[]
+  for (index, article) in enumerate(articles)
+    if !isempty(optimized_articles)
+      previous_article = optimized_articles[end]
+      if isa(previous_article, DeleteArticles) && isa(article, DeleteArticles)
+        append!(previous_article.deleted_articles, article.deleted_articles)
+        continue
+      end
+    end
+    push!(optimized_articles, article)
+  end
+
+
+  open(args["output_file"], "w") do output_file
+    println(output_file, join(
+      map(index_and_article -> text(index_and_article[2], index_and_article[1]), enumerate(optimized_articles)),
+      "\n\n",
+    ))
+  end
+end
+
+
+function parse_command_line()
+  arg_parse_settings = ArgParseSettings()
+  @add_arg_table arg_parse_settings begin
+    "--new", "-n"
+      default = "HEAD"
+      help = "target commit to compare to"
+    "--old", "-o"
+      default = "HEAD^"
+      help = "source commit to compare from"
+    "--verbose", "-v"
+      action = :store_true
+      help = "increase output verbosity"
+    "dir"
+      help = "path of Git repository containing French law"
+      required = true
+    "output_file"
+      help = "path of generated file that will contain the diff in legal wordings"
+      required = true
+  end
+  return parse_args(arg_parse_settings)
+end
+
+
+main()
 
 end # module
---
+++ src/articles.jl
@@ -0,0 +1,209 @@
+# GitLegistique.jl -- Convert Git diffs of law to french wordings
+# By: Emmanuel Raviart <emmanuel.raviart@data.gouv.fr>
+#
+# Copyright (C) 2015 Etalab
+# https://github.com/etalab/GitLegistique.jl
+#
+# The GitLegistique.jl package is licensed under the MIT "Expat" License.
+
+
+abstract Article
+abstract BuiltArticle <: Article
+
+
+type ParsedArticle <: Article
+  text::String
+  number_chars_range::Range
+  paragraphs_chars_range::Vector{Range}
+  paragraphs_lines_range::Vector{Range}
+end
+
+
+type CreateArticles <: BuiltArticle
+  created_articles::Array{ParsedArticle}
+end
+
+CreateArticles(created_article::ParsedArticle) = CreateArticles(ParsedArticle[created_article])
+
+
+type DeleteArticles <: BuiltArticle
+  deleted_articles::Array{ParsedArticle}
+end
+
+DeleteArticles(deleted_article::ParsedArticle) = DeleteArticles(ParsedArticle[deleted_article])
+
+
+type ModifyArticle <: BuiltArticle
+  old_article::ParsedArticle
+  old_paragraphs_range::Range
+  new_article::ParsedArticle
+  new_paragraphs_range::Range
+end
+
+
+function body(article::CreateArticles)
+  @assert length(article.created_articles) == 1
+  created_article = article.created_articles[1]
+  created_article_number = number(created_article)
+  paragraphs = String[
+    "Il est inséré un article $created_article_number ainsi rédigé :",
+  ]
+  append!(paragraphs,
+    map(paragraph_range -> created_article.text[paragraph_range], created_article.paragraphs_chars_range))
+  paragraphs[2] = "« Art. $created_article_number – $(paragraphs[2])"
+  paragraphs[end] = "$(paragraphs[end]) »"
+  return string(join(map(paragraph -> wrap(rstrip(paragraph); width = 80), paragraphs), "\n\n"), '\n')
+end
+
+function body(article::DeleteArticles)
+  if length(article.deleted_articles) == 1
+    return wrap("L'article $(number(article.deleted_articles[1])) est abrogé.\n"; width = 80)
+  else
+    return wrap("Les articles $(join(map(number, article.deleted_articles), ", ", " et ")) sont abrogés.\n"; width = 80)
+  end
+end
+
+function body(article::ModifyArticle)
+  paragraphs = String[]
+  if article.old_paragraphs_range.start > article.old_paragraphs_range.stop
+    # Insert paragraphs.
+    if article.old_paragraphs_range.start <= 1
+      push!(paragraphs, string(
+        "Il est inséré avant l'alinéa ",
+        article.old_paragraphs_range.start,
+        " de l'article ",
+        number(article.old_article),
+        " les disposition suivantes :",
+      ))
+    else
+      push!(paragraphs, string(
+        "Il est inséré après l'alinéa ",
+        article.old_paragraphs_range.stop,
+        " de l'article ",
+        number(article.old_article),
+        " les disposition suivantes :",
+      ))
+    end
+    append!(paragraphs,
+      map(paragraph_index -> article.new_article.text[article.new_article.paragraphs_chars_range[paragraph_index]],
+        article.new_paragraphs_range))
+    paragraphs[2] = "« $(paragraphs[2])"
+    paragraphs[end] = "$(paragraphs[end]) »"
+  elseif article.new_paragraphs_range.start > article.new_paragraphs_range.stop
+    # Delete paragraphs.
+    if article.old_paragraphs_range.start == article.old_paragraphs_range.stop
+      push!(paragraphs, string(
+        "L'alinéa ",
+        article.old_paragraphs_range.start,
+        " de l'article ",
+        number(article.old_article),
+        " est supprimé.",
+      ))
+    else
+      push!(paragraphs, string(
+        "Les alinéas ",
+        article.old_paragraphs_range.start,
+        " à ",
+        article.old_paragraphs_range.stop,
+        " de l'article ",
+        number(article.old_article),
+        " sont supprimés.",
+      ))
+    end
+  else
+    # Modify paragraphs.
+    if article.old_paragraphs_range.start == article.old_paragraphs_range.stop
+      push!(paragraphs, string(
+        "L'alinéa ",
+        article.old_paragraphs_range.start,
+        " de l'article ",
+        number(article.old_article),
+        " est remplacé par les disposition suivantes :",
+      ))
+    else
+      push!(paragraphs, string(
+        "Les alinéas ",
+        article.old_paragraphs_range.start,
+        " à ",
+        article.old_paragraphs_range.stop,
+        " de l'article ",
+        number(article.old_article),
+        " sont remplacés par les disposition suivantes :",
+      ))
+    end
+    append!(paragraphs,
+      map(paragraph_index -> article.new_article.text[article.new_article.paragraphs_chars_range[paragraph_index]],
+        article.new_paragraphs_range))
+    paragraphs[2] = "« $(paragraphs[2])"
+    paragraphs[end] = "$(paragraphs[end]) »"
+  end
+  return string(join(map(paragraph -> wrap(rstrip(paragraph); width = 80), paragraphs), "\n\n"), '\n')
+end
+
+body(article::ParsedArticle) = article.text[article.paragraphs_chars_range[1].start:end]
+
+
+function char_index_to_paragraph_index(article::ParsedArticle, char_index::Int)
+  if char_index <= 0
+    # When character index is -1 or 0, paragraph index is the same.
+    return char_index
+  end
+  if article.text[char_index] == '\n' && article.text[char_index - 1] == '\n'
+    # An empty line belongs to the previous paragraph.
+    char_index -= 2
+  end
+  for (paragraph_index, paragraph_chars_range) in enumerate(article.paragraphs_chars_range)
+    if paragraph_chars_range.start <= char_index <= paragraph_chars_range.stop
+      return paragraph_index
+    end
+  end
+  error("Paragraph not found for character at index $char_index.")
+end
+
+
+function line_index_to_paragraph_index(article::ParsedArticle, line_index::Int)
+  if line_index <= 0
+    # When line index is -1 or 0, paragraph index is the same.
+    return line_index
+  end
+  for (paragraph_index, paragraph_lines_range) in enumerate(article.paragraphs_lines_range)
+    if paragraph_lines_range.start <= line_index <= paragraph_lines_range.stop
+      return paragraph_index
+    end
+  end
+  error("Paragraph not found for line at index $line_index.")
+end
+
+
+number(article::ParsedArticle) = article.text[article.number_chars_range]
+
+
+function parse_article(text)
+  line1, line2, body = split(text, "\n", 3)
+  @assert startswith(line1, "Article ")
+  number_str = line1[sizeof("Article ") + 1:end]
+  @assert line2 == "----"
+  article = ParsedArticle(text, sizeof("Article ") + 1:sizeof(line1), Range[], Range[])
+  paragraph_chars_start_index = 1 + sizeof(line1) + 1 + sizeof(line2) + 1
+  paragraph_lines_start_index = 3
+  while paragraph_chars_start_index <= sizeof(text)
+    paragraph_separator_range = search(text, "\n\n", paragraph_chars_start_index)
+    paragraph_chars_stop_index = paragraph_separator_range.start > 0 ?
+      paragraph_separator_range.start - 1 :
+      sizeof(rstrip(text))
+    paragraph_lines_stop_index = paragraph_lines_start_index + count(char -> char == '\n',
+      text[paragraph_chars_start_index:paragraph_chars_stop_index])
+    push!(article.paragraphs_chars_range, paragraph_chars_start_index:paragraph_chars_stop_index)
+    push!(article.paragraphs_lines_range, paragraph_lines_start_index:paragraph_lines_stop_index)
+    paragraph_chars_start_index = paragraph_chars_stop_index + 3
+    paragraph_lines_start_index = paragraph_lines_stop_index + 2
+  end
+  return article
+end
+
+
+text(article::BuiltArticle, index::Int) = string(
+  "Article $(index)\n",
+  "----\n",
+  body(article),
+)
